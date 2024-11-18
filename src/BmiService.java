968abdaccf793aa67c08a676db9d3200523b8ff3
Artem Arafailov <artem.ar888@gmail.com>
2024-11-18 18:36:29 +0300

    Variables have been renamed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(double wKg, double hM) {
-    int index = (int) Math.round(wKg / Math.pow(hM, 2));
-    return index;
+    public int calculate(double weightKG, double heightM) {
+        int index = (int) Math.round(weightKG / Math.pow(heightM, 2));
+        return index;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         BmiService bmi = new BmiService();
-        double a = 98; // вес в кг
-        double b = 1.87; // рост в м
-        System.out.printf("\n Масса: %s кг \n Рост: %s м \nИндекс массы тела: %s\n", a, b, bmi.calculate(a, b));
+        double weightKG = 98; // вес в кг
+        double heightM = 1.87; // рост в м
+        System.out.printf("\n Масса: %s кг \n Рост: %s м \nИндекс массы тела: %s\n", weightKG, heightM, bmi.calculate(weightKG, heightM));
 
-        a = 50; // вес в кг
-        b = 1.3; // рост в м
-        System.out.printf("\n Масса: %s кг \n Рост: %s м \nИндекс массы тела: %s\n", a, b, bmi.calculate(a, b));
+        weightKG = 50; // вес в кг
+        heightM = 1.3; // рост в м
+        System.out.printf("\n Масса: %s кг \n Рост: %s м \nИндекс массы тела: %s\n", weightKG, heightM, bmi.calculate(weightKG, heightM));
 
-        a = 77; // вес в кг
-        b = 1.7; // рост в м
-        System.out.printf("\n Масса: %s кг \n Рост: %s м \nИндекс массы тела: %s\n", a, b, bmi.calculate(a, b));
+        weightKG = 77; // вес в кг
+        heightM = 1.7; // рост в м
+        System.out.printf("\n Масса: %s кг \n Рост: %s м \nИндекс массы тела: %s\n", weightKG, heightM, bmi.calculate(weightKG, heightM));
     }
 }
